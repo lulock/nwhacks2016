8513c9e23038822c663324b757032b7d6bb1b0e6
banafshehderayat <b.derayat@gmail.com>
2016-02-28 00:10:03 -0800

    added thumbnail for youtube

--- app.js
+++ app.js
@@ -52,6 +52,13 @@ app.controller('HomeController', function($scope, $firebaseArray, $firebase, $fi
   	});
   };
 
+
+	$scope.getId = function youtube_parser(url){
+		var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+		var match = url.match(regExp);
+		return (match&&match[7].length==11)? match[7] : false;
+	}
+
   // create a synchronized array
   $scope.rooms = $firebaseArray(ref);
 
