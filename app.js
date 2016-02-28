c4b137fdb7f3e7cfd2e1f577144b3bde033e384a
Crystal To <crystal.kyto@gmail.com>
2016-02-27 20:37:39 -0800

    able to route

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-var Firebase = require("firebase");
+var app = angular.module('theatreApp', ['firebase', 'ngRoute']);
 
-// create our angular module and inject firebase
-angular.module('theatreApp', ['firebase', 'ngRoute'])
+//routes
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/rooms/:roomId', {
+        templateUrl: 'views/room.html',
+        controller: 'RoomController'
+      }).
+      when('/', {
+      	templateUrl: 'views/home.html',
+      	controller: 'HomeController'
+      }).
+      otherwise({redirect: "/"});
+	}
+]);
 
 
 // create our main controller and get access to firebase
-.controller('mainController', function($scope, $firebase) {
+app.controller('HomeController', function($scope) {
 
-  // our application code will go here
+  console.log("home");
+
+});
+
+app.controller('RoomController', function($scope, $routeParams) {
+
+	$scope.roomId = $routeParams.roomId;
 
 });
