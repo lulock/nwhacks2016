f610edd1f62bc4a6a96431c70f2df4bf55af123c
Anaid Gakhokidze <annygakhokidze@dhcp-128-189-232-190.ubcsecure.wireless.ubc.ca>
2016-02-27 20:46:25 -0800

    Added adding of the room

--- app.js
+++ app.js
@@ -2,8 +2,30 @@
 angular.module('theatreApp', ['firebase', 'ngRoute'])
 
 // create our main controller and get access to firebase
-.controller('mainController', function($scope, $firebase) {
+.controller('mainController', function($scope, $firebaseArray, $firebase, $firebaseObject) {
 
-  // our application code will go here
+  var ref = new Firebase("https://thea2gether.firebaseio.com/rooms");
+  // create a synchronized array
+  $scope.rooms = $firebaseArray(ref);
 
+  $scope.addRoom = function(form) {
+  	console.log("Adding a room");
+  	$scope.rooms.$add({
+  		roomTitle: form.roomTitle,
+  		roomDesc: form.roomDesc,
+  		youtubeUrl: form.youtubeUrl,
+  		messages: []
+  	}).then(function(ref){
+  		// After we added the room
+  		console.log('Added a room');
+  		ref.on('value', function(snapshot) {
+  			// what to do when the value of the room changes
+  			console.log("Room was changed")
+  			console.log(snapshot.val().roomTitle);
+  		});
+  		var roomNumber = ref.name();
+  		ref.key();
+  		console.log(roomNumber);
+  	});
+  };
 });
