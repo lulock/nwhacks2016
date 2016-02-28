8632e6cf25065f10aab92227ceeb94fd2af93774
banafshehderayat <b.derayat@gmail.com>
2016-02-27 16:48:23 -0800

    fixed error

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // create our angular module and inject firebase
-angular.module('theatre', ['firebase'])
+angular.module('theatreApp', ['firebase', 'ngRoute'])
 
 // create our main controller and get access to firebase
 .controller('mainController', function($scope, $firebase) {
-  
+
   // our application code will go here
-  
+
 });
